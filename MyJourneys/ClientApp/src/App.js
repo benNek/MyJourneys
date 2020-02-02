4b2d6fdfdf4fe57054858fbb6a0f0e476d1b721d
benNek <benasnek@gmail.com>
2020-02-02 23:11:19 +0200

    NavBar Progress, Register Modal. #4 #6

--- MyJourneys/ClientApp/src/App.js
+++ MyJourneys/ClientApp/src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
-import { Layout } from './components/Layout';
+import React from 'react';
+import {Route} from 'react-router';
+import {Layout} from './components/Layout';
 import Home from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import {FetchData} from './components/FetchData';
+import {Counter} from './components/Counter';
 
-export default class App extends Component {
-  static displayName = App.name;
+export default function App() {
 
-  render () {
-    return (
+  return (
       <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Route exact path='/' component={Home}/>
+        <Route path='/counter' component={Counter}/>
+        <Route path='/fetch-data' component={FetchData}/>
       </Layout>
-    );
-  }
+  );
 }
