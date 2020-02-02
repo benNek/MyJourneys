0d9bb04c4bec68da8e9cb59986f1b743612777fa
benNek <benasnek@gmail.com>
2020-02-02 21:59:36 +0200

    Add MapBox integration. Resolve #1

--- MyJourneys/ClientApp/src/components/Home.js
+++ MyJourneys/ClientApp/src/components/Home.js
@@ -1,26 +1,82 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
+import ReactMapGL from 'react-map-gl';
+import _ from "lodash";
 
-export class Home extends Component {
-  static displayName = Home.name;
+export default function Home() {
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+  const defaultOpacity = 0.4;
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: 400,
+    latitude: 42.361145,
+    longitude: -71.057083,
+    zoom: 8
+  });
+
+  const [visitedCountries, setVisitedCountries] = useState([]);
+  const [map, setMap] = useState({});
+
+  const addVisitedCountry = (countryCode) => {
+    if (_.includes(visitedCountries, countryCode)) {
+      return;
+    }
+    setVisitedCountries([
+      ...visitedCountries, countryCode
+    ]);
+  };
+
+  useEffect(() => {
+    if (Object.entries(map).length === 0 && map.constructor === Object) {
+      return;
+    }
+    
+    const activeCountries = visitedCountries.map(country => {
+      return [country, defaultOpacity];
+    }).flat();
+    map.setPaintProperty('countries', 'fill-opacity', [
+      'match',
+      ['get', 'ADM0_A3_IS'],
+      ...activeCountries,
+      0
+    ]);
+  }, [visitedCountries]);
+
+  const loadLayers = (map) => {
+    setMap(map.target);
+    map.target.addLayer({
+      'id': 'countries',
+      'source': {
+        'type': 'vector',
+        'url': 'mapbox://byfrost-articles.74qv0xp0'
+      },
+      'source-layer': 'ne_10m_admin_0_countries-76t9ly',
+      'type': 'fill',
+      'paint': {
+        'fill-color': '#fbb03b',
+        'fill-outline-color': '#F2F2F2',
+        'fill-opacity': 0
+      }
+    });
+  };
+
+  const clickCountry = (area) => {
+    const country = _.find(area.features, ['source', 'countries']);
+    if (!country || !country.properties) {
+      return;
+    }
+
+    const code = country.properties.ADM0_A3_IS;
+    addVisitedCountry(code);
+  };
+
+  return (
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={(viewport) => setViewport(viewport)}
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      onLoad={loadLayers}
+      onClick={clickCountry}
+    />
+  );
 }
