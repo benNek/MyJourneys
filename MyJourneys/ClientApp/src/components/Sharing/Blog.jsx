af5b587c7db7abd96ee18eb477ef045b7980b270
benNek <benasnek@gmail.com>
2020-03-20 00:52:41 +0200

    Add blog sharing options. Resolves #25

--- MyJourneys/ClientApp/src/components/Sharing/Blog.jsx
+++ MyJourneys/ClientApp/src/components/Sharing/Blog.jsx
@@ -8,10 +8,29 @@ import Typography from "@material-ui/core/Typography";
 import Editor from "rich-markdown-editor";
 import Divider from "@material-ui/core/Divider";
 import moment from "moment";
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  TwitterIcon,
+  TwitterShareButton
+} from "react-share";
 
 const useStyles = makeStyles({
   divider: {
     margin: '12px 0'
+  },
+  dateSocialBlock: {
+    display: 'flex',
+    justifyContent: 'space-between'
+  },
+  social: {
+    display: 'inline-block',
+    marginLeft: '-8px'
+  },
+  socialItem: {
+    marginLeft: '8px'
   }
 });
 
@@ -19,12 +38,12 @@ export default function Blog() {
   const classes = useStyles();
   const [blog, setBlog] = useState(undefined);
 
-  let { id } = useParams();
+  let {id} = useParams();
 
   useEffect(() => {
     getBlog(id).then(res => setBlog(res.data)).catch(err => console.log(err));
   }, []);
-  
+
   if (!blog) {
     return (
       <React.Fragment/>
@@ -33,16 +52,29 @@ export default function Blog() {
   return (
     <Card variant="outlined">
       <CardContent>
-        <Typography className={classes.heading} variant="h5" component="h2">
+        <Typography className={classes.heading} variant="h3" component="h1">
           {blog.title}
         </Typography>
-        <Divider className={classes.divider} />
+        <Divider className={classes.divider}/>
         <Editor
           readOnly={true}
           defaultValue={blog.text}
         />
-        <Divider className={classes.divider} />
+        <Divider className={classes.divider}/>
+        <Typography className={classes.dateSocialBlock} variant="caption">
           {moment(blog.createDate, 'YYYY-MM-DD').fromNow()}
+          <span className={classes.social}>
+            <EmailShareButton className={classes.socialItem} url={window.location.href}>
+              <EmailIcon size={24} round={true} bgStyle={{fill: '#484848'}}/>
+            </EmailShareButton>
+            <TwitterShareButton className={classes.socialItem} url={window.location.href}>
+              <TwitterIcon size={24} round={true} bgStyle={{fill: '#484848'}}/>
+            </TwitterShareButton>
+            <FacebookShareButton className={classes.socialItem} url={window.location.href}>
+              <FacebookIcon size={24} round={true} bgStyle={{fill: '#484848'}}/>
+            </FacebookShareButton>
+          </span>
+        </Typography>
       </CardContent>
     </Card>
   )
