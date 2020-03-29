fd99932c98ee6b8ce9d94fdb936b3e1dbf1f09b7
benNek <benasnek@gmail.com>
2020-03-29 15:31:36 +0300

    Add tabs for planner. Solves #41

---
+++ MyJourneys/ClientApp/src/components/Planner/Itinerary.jsx
@@ -0,0 +1,137 @@
+import Typography from "@material-ui/core/Typography";
+import _ from "lodash";
+import moment from "moment";
+import React from "react";
+import {ITEM_TYPE_EVENT, ITEM_TYPE_FLIGHT, ITEM_TYPE_HOTEL, ITEM_TYPE_RESERVATION} from "../../types/journeyItemTypes";
+import FlightItemCard from "./FlightItemCard";
+import HotelItemCard from "./HotelItemCard";
+import ReservationItemCard from "./ReservationItemCard";
+import EventItemCard from "./EventItemCard";
+import FlightIcon from "@material-ui/icons/Flight";
+import HotelIcon from "@material-ui/icons/Hotel";
+import RestaurantIcon from "@material-ui/icons/Restaurant";
+import EventIcon from "@material-ui/icons/Event";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import {useParams} from "react-router";
+
+const useStyles = makeStyles(() => ({
+  day: {
+    margin: '6px 0'
+  },
+  item: {
+    display: 'flex'
+  },
+  iconContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    position: 'relative',
+    alignSelf: 'stretch',
+    marginRight: '24px',
+  },
+  line: {
+    outline: '1px solid #E0E0E0',
+    position: 'absolute',
+    left: 'calc(50%)',
+    top: 0,
+    bottom: 0
+  },
+  icon: {
+    display: 'flex',
+    flexShrink: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
+    borderRadius: '50%',
+    background: 'white',
+    color: 'rgba(0, 0, 0, 0.54)',
+    width: '40px',
+    height: '40px',
+    boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)'
+  }
+}));
+
+export default function Itinerary(props) {
+  let {location} = useParams();
+  const {items} = props;
+
+  const classes = useStyles();
+
+  const getItem = item => {
+    switch (item.type) {
+      case ITEM_TYPE_FLIGHT:
+        return <FlightItemCard flight={item}/>;
+      case ITEM_TYPE_HOTEL:
+        return <HotelItemCard hotel={item}/>;
+      case ITEM_TYPE_RESERVATION:
+        return <ReservationItemCard reservation={item}/>;
+      case ITEM_TYPE_EVENT:
+        return <EventItemCard event={item}/>;
+      default:
+        return <p>Invalid item type</p>;
+    }
+  };
+
+  const getIcon = item => {
+    let icon;
+    switch (item.type) {
+      case ITEM_TYPE_FLIGHT:
+        icon = <FlightIcon/>;
+        break;
+      case ITEM_TYPE_HOTEL:
+        icon = <HotelIcon/>;
+        break;
+      case ITEM_TYPE_RESERVATION:
+        icon = <RestaurantIcon/>;
+        break;
+      case ITEM_TYPE_EVENT:
+        icon = <EventIcon/>;
+        break;
+      default:
+        icon = <FlightIcon/>
+    }
+
+    return (
+      <div className={classes.iconContainer}>
+        <div className={classes.line}/>
+        <div className={classes.icon}>
+          {icon}
+        </div>
+      </div>
+    );
+  };
+
+  const getItems = () => {
+    if (!items.length) {
+      return (
+        <Typography variant="body1">
+          {location} journey has no items added! Click button in bottom right to get started!
+        </Typography>
+      )
+    }
+
+    const groupedByDays = _.groupBy(items, item => moment(item.date).format('MMMM Do'));
+    return Object.keys(groupedByDays).map(day => {
+      const items = groupedByDays[day].map(item => {
+        return (
+          <div className={classes.item} key={`${item.type}-${item.id}`}>
+            {getIcon(item)}
+            {getItem(item)}
+          </div>
+        )
+      });
+      return (
+        <React.Fragment key={day}>
+          <Typography className={classes.day} variant='h6'>{day}</Typography>
+          {items}
+        </React.Fragment>
+      )
+    });
+  };
+
+  return (
+    <React.Fragment>
+      {getItems()}
+    </React.Fragment>
+  )
+
+}
--- MyJourneys/ClientApp/src/components/Planner/Journey.jsx
+++ MyJourneys/ClientApp/src/components/Planner/Journey.jsx
@@ -1,148 +1,47 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import OfflinePinIcon from '@material-ui/icons/OfflinePin';
 import JourneyItemsSpeedDial from "./JourneyItemsSpeedDial";
+import Tabs from "@material-ui/core/Tabs";
+import Paper from "@material-ui/core/Paper";
+import Tab from "@material-ui/core/Tab";
+import Box from "@material-ui/core/Box";
+import Itinerary from "./Itinerary";
 import {getJourneyItems} from "../../utils/networkFunctions";
 import {toast} from "react-toastify";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import {ITEM_TYPE_EVENT, ITEM_TYPE_FLIGHT, ITEM_TYPE_HOTEL, ITEM_TYPE_RESERVATION} from "../../types/journeyItemTypes";
-import FlightItemCard from "./FlightItemCard";
-import HotelItemCard from "./HotelItemCard";
-import ReservationItemCard from "./ReservationItemCard";
-import EventItemCard from "./EventItemCard";
-import makeStyles from "@material-ui/core/styles/makeStyles";
-import _ from "lodash";
-import moment from "moment";
-import FlightIcon from '@material-ui/icons/Flight';
-import HotelIcon from '@material-ui/icons/Hotel';
-import RestaurantIcon from '@material-ui/icons/Restaurant';
-import EventIcon from '@material-ui/icons/Event';
-
-const useStyles = makeStyles(() => ({
-  day: {
-    margin: '6px 0'
-  },
-  item: {
-    display: 'flex'
-  },
-  iconContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    position: 'relative',
-    alignSelf: 'stretch',
-    marginRight: '24px',
-  },
-  line: {
-    outline: '1px solid #E0E0E0',
-    position: 'absolute',
-    left: 'calc(50%)',
-    top: 0,
-    bottom: 0
-  },
-  icon: {
-    display: 'flex',
-    flexShrink: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
-    zIndex: 1,
-    borderRadius: '50%',
-    background: 'white',
-    color: 'rgba(0, 0, 0, 0.54)',
-    width: '40px',
-    height: '40px',
-    boxShadow: '0px 3px 5px -1px rgba(0,0,0,0.2), 0px 6px 10px 0px rgba(0,0,0,0.14), 0px 1px 18px 0px rgba(0,0,0,0.12)'
-  }
-}));
 
 export default function Journey() {
-  const classes = useStyles();
   let {location, id} = useParams();
-  const [loading, setLoading] = useState(true);
+  const [tab, setTab] = React.useState(0);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     getJourneyItems(id).then(res => {
-      setLoading(false);
       setItems(res.data);
     }).catch(err => toast.error(err));
   }, []);
 
-  const getIcon = item => {
-    let icon;
-    switch (item.type) {
-      case ITEM_TYPE_FLIGHT:
-        icon = <FlightIcon/>;
-        break;
-      case ITEM_TYPE_HOTEL:
-        icon = <HotelIcon/>;
-        break;
-      case ITEM_TYPE_RESERVATION:
-        icon = <RestaurantIcon/>;
-        break;
-      case ITEM_TYPE_EVENT:
-        icon = <EventIcon/>;
-        break;
-      default:
-        icon = <FlightIcon/>
-    }
-    
+  const handleTabChange = (event, newValue) => {
+    setTab(newValue);
+  };
+
+  function TabPanel(props) {
+    const {children, value, index, ...other} = props;
+
     return (
-      <div className={classes.iconContainer}>
-        <div className={classes.line}/>
-        <div className={classes.icon}>
-          {icon}
-        </div>
-      </div>
+      <Typography
+        component="div"
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && <Box p={3}>{children}</Box>}
+      </Typography>
     );
-  };
-
-  const getItem = item => {
-    switch (item.type) {
-      case ITEM_TYPE_FLIGHT:
-        return <FlightItemCard flight={item}/>;
-      case ITEM_TYPE_HOTEL:
-        return <HotelItemCard hotel={item}/>;
-      case ITEM_TYPE_RESERVATION:
-        return <ReservationItemCard reservation={item}/>;
-      case ITEM_TYPE_EVENT:
-        return <EventItemCard event={item}/>;
-      default:
-        return <p>Invalid item type</p>;
-    }
-  };
-
-  const getItems = () => {
-    if (loading) {
-      return (<CircularProgress className={"Loader"}/>)
-    }
-    if (!items.length) {
-      return (
-        <Typography variant="body1">
-          {location} journey has no items added! Click button in bottom right to get started!
-        </Typography>
-      )
-    }
-
-    const groupedByDays = _.groupBy(items, item => moment(item.date).format('MMMM Do'));
-    return Object.keys(groupedByDays).map(day => {
-      const items = groupedByDays[day].map(item => {
-        return (
-          <div className={classes.item} key={`${item.type}-${item.id}`}>
-            {getIcon(item)}
-            {getItem(item)}
-          </div>
-        )
-      });
-      return (
-        <React.Fragment key={day}>
-          <Typography className={classes.day} variant='h6'>{day}</Typography>
-          {items}
-        </React.Fragment>
-      )
-    });
-  };
+  }
 
   return (
     <React.Fragment>
@@ -152,8 +51,28 @@ export default function Journey() {
           <OfflinePinIcon/>
         </span>
       </Typography>
-      <Divider/>
-      {getItems()}
+      <Paper>
+        <Tabs
+          value={tab}
+          onChange={handleTabChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+        >
+          <Tab label="Itinerary"/>
+          <Tab label="Places"/>
+          <Tab label="Notes"/>
+        </Tabs>
+      </Paper>
+      <TabPanel value={tab} index={0}>
+        <Itinerary items={items}/>
+      </TabPanel>
+      <TabPanel value={tab} index={1}>
+        Places
+      </TabPanel>
+      <TabPanel value={tab} index={2}>
+        Notes
+      </TabPanel>
       <JourneyItemsSpeedDial journeyId={id}/>
     </React.Fragment>
   )
