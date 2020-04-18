21800d823fdfbac065b411a2c8dcac2b13b0cea8
benNek <benasnek@gmail.com>
2020-04-19 00:46:23 +0300

    Improve look of article form. #94

--- MyJourneys/ClientApp/src/components/Sharing/ArticleForm.jsx
+++ MyJourneys/ClientApp/src/components/Sharing/ArticleForm.jsx
@@ -21,6 +21,8 @@ import VisibilityOutlinedIcon from '@material-ui/icons/VisibilityOutlined';
 import SpeakerNotesIcon from '@material-ui/icons/SpeakerNotes';
 import CreateOutlinedIcon from '@material-ui/icons/CreateOutlined';
 import RecommendedAction from "../RecommendedAction";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
 
 const useStyles = makeStyles(theme => ({
   successContainer: {
@@ -93,118 +95,120 @@ export default function ArticleForm() {
   }
 
   return (
-    <Fragment>
-      <Typography component='h1' variant='h3'>
-        Publish your article
-      </Typography>
-      <Divider/>
-      <Formik
-        initialValues={{title: '', tags: [], text: ' '}}
-        validationSchema={articleValidation}
-        onSubmit={async (values, actions) => {
-          if (!isSubmitting) {
-            return;
-          }
+    <Card variant="outlined">
+      <CardContent>
+        <Typography component='h1' variant='h3'>
+          Publish your article
+        </Typography>
+        <Divider/>
+        <Formik
+          initialValues={{title: '', tags: [], text: ' '}}
+          validationSchema={articleValidation}
+          onSubmit={async (values, actions) => {
+            if (!isSubmitting) {
+              return;
+            }
 
-          const strippedText = removeMd(values['text']).replace('\\', '').trim();
-          if (!strippedText) {
-            handleOpenPopover(editorRef.current.element);
-            return;
-          }
+            const strippedText = removeMd(values['text']).replace('\\', '').trim();
+            if (!strippedText) {
+              handleOpenPopover(editorRef.current.element);
+              return;
+            }
 
-          actions.setSubmitting(true);
-          await createArticle(values)
-            .then(response => {
-              setCreatedId(response.data.id);
-              setSuccess(true);
-            })
-            .catch(err => {
-              toast.error(`${err.response.data} Status code: ${err.response.status}`);
-              actions.setSubmitting(false);
-            });
-          setIsSubmitting(false);
-        }}
-      >
-        {(formProps) => {
-          const {handleChange, setFieldTouched, setFieldValue, errors, touched} = formProps;
-          const change = (name, e) => {
-            e.persist();
-            handleChange(e);
-            setFieldTouched(name, true, false);
-          };
-          return (
-            <Form>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  <TextField
-                    autoComplete="off"
-                    name="title"
-                    required
-                    fullWidth
-                    id="title"
-                    label="Title"
-                    autoFocus
-                    error={errors.title && touched.title}
-                    helperText={(errors.title && touched.title) && errors.title}
-                    onChange={change.bind(null, "title")}
-                  />
+            actions.setSubmitting(true);
+            await createArticle(values)
+              .then(response => {
+                setCreatedId(response.data.id);
+                setSuccess(true);
+              })
+              .catch(err => {
+                toast.error(`${err.response.data} Status code: ${err.response.status}`);
+                actions.setSubmitting(false);
+              });
+            setIsSubmitting(false);
+          }}
+        >
+          {(formProps) => {
+            const {handleChange, setFieldTouched, setFieldValue, errors, touched} = formProps;
+            const change = (name, e) => {
+              e.persist();
+              handleChange(e);
+              setFieldTouched(name, true, false);
+            };
+            return (
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      autoComplete="off"
+                      name="title"
+                      required
+                      fullWidth
+                      id="title"
+                      label="Title"
+                      autoFocus
+                      error={errors.title && touched.title}
+                      helperText={(errors.title && touched.title) && errors.title}
+                      onChange={change.bind(null, "title")}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Autocomplete
+                      multiple
+                      id="tags"
+                      options={tags.map((option) => option)}
+                      freeSolo
+                      renderTags={(value, getTagProps) =>
+                        value.map((option, index) => (
+                          <Chip variant="outlined" label={option} {...getTagProps({index})} />
+                        ))
+                      }
+                      renderInput={(params) => (
+                        <TextField {...params} label="Tags"/>
+                      )}
+                      onChange={(event, values) => setFieldValue('tags', values)}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Editor
+                      ref={editorRef}
+                      bodyPlaceholder="Express your thoughts in here!"
+                      onChange={value => setFieldValue('text', value())}
+                      dark={darkMode}
+                    />
+                    <Popover
+                      anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                      }}
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                      }}
+                      anchorEl={anchorEl}
+                      open={Boolean(anchorEl)}
+                      onClose={handleClosePopover}
+                    >
+                      You must enter text!
+                    </Popover>
+                  </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                  <Autocomplete
-                    multiple
-                    id="tags"
-                    options={tags.map((option) => option)}
-                    freeSolo
-                    renderTags={(value, getTagProps) =>
-                      value.map((option, index) => (
-                        <Chip variant="outlined" label={option} {...getTagProps({index})} />
-                      ))
-                    }
-                    renderInput={(params) => (
-                      <TextField {...params} label="Tags"/>
-                    )}
-                    onChange={(event, values) => setFieldValue('tags', values)}
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Editor
-                    ref={editorRef}
-                    bodyPlaceholder="Express your thoughts in here!"
-                    onChange={value => setFieldValue('text', value())}
-                    dark={darkMode}
-                  />
-                  <Popover
-                    anchorOrigin={{
-                      vertical: 'bottom',
-                      horizontal: 'left',
-                    }}
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'left',
-                    }}
-                    anchorEl={anchorEl}
-                    open={Boolean(anchorEl)}
-                    onClose={handleClosePopover}
-                  >
-                    You must enter text!
-                  </Popover>
-                </Grid>
-              </Grid>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                color="default"
-                className={classes.submitButton}
-                endIcon={<Icon>send</Icon>}
-                onClick={() => setIsSubmitting(true)}
-              >
-                Submit
-              </Button>
-            </Form>
-          );
-        }}
-      </Formik>
-    </Fragment>
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="default"
+                  className={classes.submitButton}
+                  endIcon={<Icon>send</Icon>}
+                  onClick={() => setIsSubmitting(true)}
+                >
+                  Submit
+                </Button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </CardContent>
+    </Card>
   )
 }
