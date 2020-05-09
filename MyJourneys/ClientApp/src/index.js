037a32b896c083cd6b31761d116054606b93f65b
benNek <benasnek@gmail.com>
2020-05-09 14:45:27 +0300

    Add sentry support

--- MyJourneys/ClientApp/src/index.js
+++ MyJourneys/ClientApp/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Store from "./state/store";
@@ -9,6 +10,8 @@ require('dotenv').config();
 
 const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
+
+Sentry.init({dsn: process.env.REACT_APP_SENTRY_DSN});
 
 ReactDOM.render(
   <BrowserRouter basename={baseUrl}>
--- MyJourneys/Program.cs
+++ MyJourneys/Program.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace MyJourneys
 {
@@ -10,8 +10,12 @@ namespace MyJourneys
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseSentry();
+                });
     }
 }
